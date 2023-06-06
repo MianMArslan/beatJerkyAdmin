f1efcd9182ff15e6400cbe8eaa1bb858f5fc00fd
Hamza Yousaf <hamzay050@gmail.com>
2023-06-06 23:03:44 +0500

    bugfixed background color

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../themes/theme'; // Import your custom theme
-import profile from '../styles/profile.css'
+import '../styles/profile.css';
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }

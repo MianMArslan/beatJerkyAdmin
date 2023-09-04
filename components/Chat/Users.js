042b446a34e1154ebf07473dd81ca2939b120374
Hamza Yousaf <hamzay050@gmail.com>
2023-09-05 02:02:01 +0500

    chat bugfixed user avatar finalized

--- components/Chat/Users.js
+++ components/Chat/Users.js
@@ -43,6 +43,34 @@ const BlinkingBadge = styled(Badge)(({ theme }) => ({
     },
   },
 }));
+const BlinkingBadgeOnline = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: theme.palette.success.main, // Red color for offline users
+    color: theme.palette.success.main,
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 export default function AlignItemsList() {
   const { selectedUser, setSelectedUser } = useContext(AppContext);
@@ -96,7 +124,7 @@ export default function AlignItemsList() {
                 >
                   <ListItemAvatar>
                     {user.sender.isOnline ? (
-                      <BlinkingBadge
+                      <BlinkingBadgeOnline
                         overlap="circular"
                         anchorOrigin={{
                           vertical: "bottom",
@@ -108,7 +136,7 @@ export default function AlignItemsList() {
                           alt={user.sender.email}
                           src={user.sender.profileImg}
                         />
-                      </BlinkingBadge>
+                      </BlinkingBadgeOnline>
                     ) : (
                       <BlinkingBadge
                         overlap="circular"
